5b812a2c1acde7da89eb28e5533615576f70e663
swarajd <swaraj.dhumne@gmail.com>
2020-04-28 10:50:20 -0400

    moving config read + adding RR

--- main.py
+++ main.py
@@ -2,24 +2,21 @@ from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 from http.client import HTTPConnection
 import json
 
+from methods import RoundRobin
+
 # some sane defaults
 HOSTNAME = "localhost"
 PORT     = 8080
 
-# load config
-with open("config.json", "r") as conf:
-    config = json.loads(conf.read())
-
-# reading hosts
-host_index = 0
-hosts = config["hosts"]
-
 # handler class for all incoming HTTP requests
 class LoadBalancer(BaseHTTPRequestHandler):
     def do_GET(self):
+        global round_robin
 
         print(self.client_address)
         print(self.path)
+
+        round_robin.incr()
 
         conn = HTTPConnection("www.example.com", 80)
         # print(conn)
@@ -31,6 +28,16 @@ class LoadBalancer(BaseHTTPRequestHandler):
         self.end_headers()
 
 if __name__ == "__main__":
+    # load config
+    with open("config.json", "r") as conf:
+        config = json.loads(conf.read())
+
+    # reading hosts
+    host_index = 0
+    hosts = config["hosts"]
+
+    round_robin = RoundRobin(hosts)
+
     load_balancer = ThreadingHTTPServer((HOSTNAME, PORT), LoadBalancer)
     print(f"Server started: http://{HOSTNAME}:{PORT}")
 
