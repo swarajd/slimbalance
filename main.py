b3c450208681811f6bb9cef2f0f2b4d8aa24bed5
swarajd <swaraj.dhumne@gmail.com>
2020-04-28 01:20:55 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+from http.client import HTTPConnection
+
+HOSTNAME = "localhost"
+PORT     = 8080
+
+class LoadBalancer(BaseHTTPRequestHandler):
+    def do_GET(self):
+
+        print(self.client_address)
+        print(self.path)
+
+        conn = HTTPConnection('www.example.com', 80)
+        # print(conn)
+        conn.request("GET", "/")
+        resp = conn.getresponse()
+        # print(resp.read())
+
+        self.send_response(200)
+        self.end_headers()
+
+if __name__ == '__main__':
+    load_balancer = ThreadingHTTPServer((HOSTNAME, PORT), LoadBalancer)
+    print(f"Server started: http://{HOSTNAME}:{PORT}")
+
+    try:
+        load_balancer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    load_balancer.server_close()
+    print("Server stopped.")
