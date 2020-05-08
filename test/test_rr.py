3e733e695ceac9e8b6b605d541fdbe35bce09bd3
swarajd <swaraj.dhumne@gmail.com>
2020-05-08 19:08:46 -0400

    added test for one dead backend

--- test/test_rr.py
+++ test/test_rr.py
@@ -56,3 +56,57 @@ def test_rr_ctx_none_dead():
 
     for count in counts.values():
         assert count == LOOP_COUNT
+
+def test_rr_ctx_one_dead():
+    """
+    testing the round robin context to make sure
+    that it picks each backend equally
+    """
+
+    LOOP_COUNT = 10
+    DEAD_BACKEND = 2
+    EXPECTED_COUNT = 15
+
+    # set up backends
+    backends = [Backend({"host": f"backend-{x}"}, alive=True) for x in range(3)]
+
+    backends[DEAD_BACKEND].set_alive(False)
+
+    # create context and queue for results
+    test_ctx = RoundRobinContext(backends)
+    result_queue = queue.Queue()
+
+    # create threads
+    threads = [
+        threading.Thread(target=rr_thread, args=(test_ctx, result_queue, LOOP_COUNT))
+        for backend in backends
+    ]
+
+    # run threads
+    for thread in threads:
+        thread.start()
+
+    # join threads
+    for thread in threads:
+        thread.join()
+
+    # gather results
+    results = []
+
+    while not result_queue.empty():
+        results.extend(result_queue.get())
+
+    # check if the counts for all the hosts are equal
+    counts = defaultdict(int)
+
+    for backend in backends:
+        counts[backend.host] = 0
+
+    for result in results:
+        counts[result.host] += 1
+
+    for idx, count in enumerate(counts.values()):
+        if idx == DEAD_BACKEND:
+            assert count == 0
+        else:
+            assert count == EXPECTED_COUNT
