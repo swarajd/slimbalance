a0b386ac1bafe1a87e1cd74d7eb081ec9e2e6102
swarajd <swaraj.dhumne@gmail.com>
2020-05-10 00:29:42 -0400

    added PUT test

--- test/test_util.py
+++ test/test_util.py
@@ -118,7 +118,6 @@ def test_handler_request_data(mock_http_server, mock_connection):
     )
 
     request_str = f"{REQUEST_COMMAND} {REQUEST_PATH} HTTP/1.1\n{REQUEST_HEADER_STR}\n\n{REQUEST_CONTENT}"
-    # print(request_str)
     request_bytes = bytes(request_str, "utf-8")
 
     mock_request = Mock()
@@ -150,6 +149,71 @@ def test_handler_request_data(mock_http_server, mock_connection):
 
     handler = HandlerClass(mock_request, (REQUEST_HOST, REQUEST_PORT), mock_http_server)
 
+    assert handler.command == REQUEST_COMMAND
+    assert handler.path == REQUEST_PATH
+    for header in handler.headers:
+        assert REQUEST_HEADERS[header] == handler.headers[header]
+    assert handler.body == REQUEST_CONTENT_BYTES
+
+
+# TODO: abstract out the setup process somehow
+@patch("methods.util.HTTPConnection")
+@patch("http.server.ThreadingHTTPServer")
+def test_handler_put(mock_http_server, mock_connection):
+    """
+    testing PUT method
+    """
+
+    # set up request
+    REQUEST_COMMAND = "PUT"
+    REQUEST_PATH = "/"
+    REQUEST_CONTENT = "a=1&b=2"
+    REQUEST_CONTENT_BYTES = bytes(REQUEST_CONTENT, "utf-8")
+    REQUEST_HEADERS = {
+        "Content-Length": str(len(REQUEST_CONTENT)),
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+
+    REQUEST_HEADER_STR = "\n".join(
+        [f"{key}: {val}" for key, val in REQUEST_HEADERS.items()]
+    )
+
+    request_str = f"{REQUEST_COMMAND} {REQUEST_PATH} HTTP/1.1\n{REQUEST_HEADER_STR}\n\n{REQUEST_CONTENT}"
+    request_bytes = bytes(request_str, "utf-8")
+
+    mock_request = Mock()
+    mock_request.makefile.return_value = io.BytesIO(request_bytes)
+
+    # set up response
+    RESPONSE_STATUS = 200
+    RESPONSE_HEADERS = [("c", "d"), ("e", "f")]
+    RESPONSE_CONTENT = b"dummy data"
+
+    conn = MagicMock()
+    conn.getresponse.return_value = DummyResponse(
+        RESPONSE_STATUS, RESPONSE_HEADERS, RESPONSE_CONTENT
+    )
+
+    mock_connection.return_value = conn
+
+    # set up backend
+    BACKEND_HOST = "localhost"
+    BACKEND_PORT = 8081
+
+    context_mock = MagicMock()
+    context_mock.get_next_backend.return_value = Backend(
+        {"host": BACKEND_HOST, "port": BACKEND_PORT}, alive=True
+    )
+
+    # set up handler
+    REQUEST_HOST = "localhost"
+    REQUEST_PORT = 8080
+
+    HandlerClass = partial(LoadBalancerHandler, context_mock)
+
+    handler = HandlerClass(mock_request, (REQUEST_HOST, REQUEST_PORT), mock_http_server)
+
+    # make assertions
     assert handler.command == REQUEST_COMMAND
     assert handler.path == REQUEST_PATH
     for header in handler.headers:
