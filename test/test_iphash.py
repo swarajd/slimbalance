cd1fb7c770eddbbf215ddaa706b4a1879e7ac78c
swarajd <swaraj.dhumne@gmail.com>
2020-12-01 15:59:23 -0500

    updating tests to achieve 100% coverage

--- test/test_iphash.py
+++ test/test_iphash.py
@@ -45,3 +45,22 @@ def test_ip_hash_ctx_one_dead():
 
     cur_backend = test_ctx.get_next_backend(context)
     assert cur_backend == backends[DEAD_BACKEND + 1]
+
+def test_ip_hash_ctx_all_dead():
+
+    """
+    testing the ip hashing context to make sure
+    that it returns None if no backends are available
+    """
+
+    # set up backends
+    backends = [Backend({"host": f"backend-{x}"}, alive=False) for x in range(3)]
+
+    # create context 
+    test_ctx = IPHashContext(backends)
+
+    # dummy context
+    context = {"address": ("127.0.0.1", 80)}
+
+    backend = test_ctx.get_next_backend(context)
+    assert backend is None
